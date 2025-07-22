1c7fad6ed22c55dddc277c0f7a0019b8e3dacaac
santiago <sreyesnino@gmail.com>
2025-07-22 11:35:39 -0500

    Organizador de archivos

---
+++ menu.py
@@ -0,0 +1,27 @@
+import os 
+import shutil
+carpeta = r"C:\Users\santi\Downloads"
+archivos = os.listdir(carpeta)
+print(archivos)
+
+tiposArchivos={
+    "Imagenes": [".png",".jpg",".jpeg"],
+    "Words" : [".doc",".docx"],
+    "Excels" : [".xlsx"],
+    "Documentos" : [".pdf", ".txt"],
+    "Videos": [".mp4" , ".avi"],
+    "Musica" : [".mp3", ".wav"],
+    "Compresos" : [".zip" , ".rar"],
+}
+for archivo in archivos:
+    nombre,ext=os.path.splitext(archivo)
+    for categoria, extensiones in tiposArchivos.items():
+        if ext.lower() in extensiones:
+            nueva_ruta = os.path.join(carpeta,categoria)
+            if not os.path.exists(nueva_ruta):
+                os.mkdir(nueva_ruta)
+            origen = os.path.join(carpeta,archivo)
+            destino = os.path.join(nueva_ruta,archivo)
+            shutil.move(origen, destino)
+            print(f"Movido: {archivo}->{categoria}")
+            break
